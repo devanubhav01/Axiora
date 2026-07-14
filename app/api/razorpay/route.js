f520f056be637364e93e151952eb75cf8384dc23
devanubhav01 <anubhavsingh18599@gmail.com>
2026-07-14 16:21:58 +0530

    Add 303 redirect status to payment responses

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -5,12 +5,10 @@ import Resume from "@/models/Resume";
 import { handleResumeGenerationAndEmail } from "@/actions/resumeHelper";
 import connectDb from "@/db/connectDb";
 import User from "@/models/User";
-
 export const POST = async (req) => {
     await connectDb()
     let body = await req.formData()
     body = Object.fromEntries(body)
-
     let isResume = false;
     let secret = "";
     let p = await Payment.findOne({oid: body.razorpay_order_id})
@@ -28,10 +26,8 @@ export const POST = async (req) => {
             return NextResponse.json({success: false, message:"Order Id not found"})
         }
     }
-
     // Verify the payment
     let xx = validatePaymentVerification({"order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id}, body.razorpay_signature, secret)
-
     if(xx){
         if (isResume) {
             // Update the resume status
@@ -39,14 +35,13 @@ export const POST = async (req) => {
             // Generate PDF and send Email
             await handleResumeGenerationAndEmail(updatedResume);
             
-            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/create-resume?success=true&id=${updatedResume._id}`)
+            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/create-resume?success=true&id=${updatedResume._id}`, 303)
         } else {
             // Update the payment status
             const updatedPayment = await Payment.findOneAndUpdate({oid: body.razorpay_order_id}, {done: true}, {new: true})
-            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone=true`)  
+            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone=true`, 303)  
         }
     }
-
     else{
         return NextResponse.json({success: false, message:"Payment Verification Failed"})
     }
